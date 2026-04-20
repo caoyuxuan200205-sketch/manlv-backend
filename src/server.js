e706ee5044dcce587936d2a4cfe70e7f5759cae1
Yuxuan Cao <caoyuxuan200205@gmail.com>
2026-04-20 09:52:09 +0800

    docs: sync AI technical documentation for competition

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-﻿﻿const express = require('express');
+﻿const express = require('express');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
